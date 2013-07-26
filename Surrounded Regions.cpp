702d52611d8cb35593a47c63f9a058eee65fc3e4
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2013-07-26 12:56:12 +0800

    avoid re-enqueue node enqueued

--- Surrounded Regions.cpp
+++ Surrounded Regions.cpp
@@ -8,17 +8,27 @@ private:
     int width;
     int height;
     
+    void visit(pair<int, int>& p) {
+        area.insert(p);
+
+        int i = p.first;
+        int j = p.second;
+
+        if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
+            isOpenArea = true;
+        }
+    }
     void enqueue_if_needed(int i, int j, vector<vector<char>> &board){
+
         if (i < 0 || i >= width || j < 0 || j >= height) return;
+
         if (board[i][j] == 'O' && set_v.find(make_pair(i, j)) == set_v.end()) {
                 
-            set_v.insert(make_pair(i, j));
             
+            set_v.insert(make_pair(i, j));
+
             q.push(make_pair(i, j));
             
-            if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
-                isOpenArea = true;
-            }
             
         }
         return;
@@ -33,7 +43,7 @@ private:
         while(q.size() > 0) {
             pair<int, int> point = q.front();
             q.pop();
-            area.insert(point);
+            visit(point);
             enqueue_if_needed(point.first-1, point.second, board);
             enqueue_if_needed(point.first+1, point.second, board);
             enqueue_if_needed(point.first, point.second-1, board);
