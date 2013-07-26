fc26d54210f2b03be316e19478c9272c2b8b8b0f
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2013-07-26 12:43:51 +0800

    add surrounded regions

---
+++ Surrounded Regions.cpp
@@ -0,0 +1,82 @@
+class Solution {
+private:
+
+    set<pair<int, int>> set_v;
+    set<pair<int, int>> area;
+    queue<pair<int, int>> q;
+    bool isOpenArea;
+    int width;
+    int height;
+    
+    void enqueue_if_needed(int i, int j, vector<vector<char>> &board){
+        if (i < 0 || i >= width || j < 0 || j >= height) return;
+        if (board[i][j] == 'O' && set_v.find(make_pair(i, j)) == set_v.end()) {
+                
+            set_v.insert(make_pair(i, j));
+            
+            q.push(make_pair(i, j));
+            
+            if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
+                isOpenArea = true;
+            }
+            
+        }
+        return;
+    }
+    void searchAt(int i, int j, vector<vector<char>> &board) {
+        
+        isOpenArea = false;
+        
+        enqueue_if_needed(i, j, board);
+        
+        
+        while(q.size() > 0) {
+            pair<int, int> point = q.front();
+            q.pop();
+            area.insert(point);
+            enqueue_if_needed(point.first-1, point.second, board);
+            enqueue_if_needed(point.first+1, point.second, board);
+            enqueue_if_needed(point.first, point.second-1, board);
+            enqueue_if_needed(point.first, point.second+1, board);
+        }
+        
+        if (!isOpenArea) {
+            set<pair<int, int>>::iterator i = area.begin();
+            while(i != area.end()) {
+                pair<int, int> point = *i;
+                board[point.first][point.second] = 'X';
+                i++;
+            }
+        }
+        area.clear();
+    }
+public:
+
+    void solve(vector<vector<char>> &board) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        isOpenArea = false;
+
+        height = board.size();
+        
+        if (height <= 0) return;
+        
+        width = board[0].size();
+        
+        
+        for (int i = 1; i < board.size() - 1; i++) {
+            
+            vector<char> &row = board[i];
+            
+            for (int j = 1; j < row.size() - 1; j++) {
+                
+                if (row[j] == 'O')
+                    
+                    searchAt(i, j, board);
+            }
+        }
+        
+        set_v.clear();
+        isOpenArea = false;
+    }
+};
---
+++ palindrome_partitioningII.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int* count;
+    bool isPalindrome(const string &s, int start, int index){
+        int i = start, j = index - 1;
+        
+        while (i < j){
+            if (s[i] != s[j]){
+                //cout << s[i] << s[j] << i << j << endl;
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+    int minCutAt(int index, const string& s){
+        if (index < s.size()){
+            
+            if (count[index] < 0){
+                
+                
+                if (isPalindrome(s, index, s.size()))
+                {
+                    count[index] = 0;
+                    return 0;
+                }
+
+                int minThrehold = s.size() - index;
+                for (int i = s.size() - 1; i > index; i--){
+                
+                
+                    if (isPalindrome(s, index, i)) {
+                    
+                        minThrehold = min(minCutAt(i, s) + 1, minThrehold);
+                        if (minThrehold <= 1) break;
+                    }
+                }
+                count[index] = minThrehold;
+                
+            }
+            return count[index];
+             
+        }
+        return 0;
+    }
+    int minCut(string& s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+      
+            
+            count = new int[s.size()];
+            for (int i = 0; i < s.size(); i++){
+                count[i] = -1;
+            }
+      
+        
+        return minCutAt(0, s);
+    }
+};
